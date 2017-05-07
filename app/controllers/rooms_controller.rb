ae77225740f580966582441d956cb71f4be517fc
Rodman Aguirre Méndez <rodfagui@gmail.com>
2017-05-07 12:08:13 -0500

    Añade la funcionalidad de editar habitaciones

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -16,6 +16,19 @@ class RoomsController < ApplicationController
     end
   end
 
+  def edit
+    @room = Room.find(params[:id])
+  end
+
+  def update
+    @room = Room.find(params[:id])
+    if @room.update(room_params)
+      redirect_to rooms_path
+    else
+      render :edit
+    end
+  end
+
   protected
     def room_params
       params.require(:room).permit(:title, :description, :beds, :guests, :image_url)
